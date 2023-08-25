607fbd6784ce8b9f6f2fe106937fabab4b715fe8
OttoDragon <103608368+OttoDragon@users.noreply.github.com>
2023-08-25 19:22:40 -0300

    Add files via upload

---
+++ c-103-main/c-103/c-103.py
@@ -0,0 +1,42 @@
+import sys
+import time
+import random
+import os
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+from_dir = "C:/Users/tvgge/OneDrive/Área de Trabalho"
+
+class fileEventHandler(FileSystemEventHandler):
+    
+    def on_created(self, event):
+        print(f'ola, {event.src_path} foi criado!')
+
+    def on_deleted(self, event):
+        print(f'opa! Alguém excluiu {event.src_path}!')
+
+
+event_handler = fileEventHandler()
+
+observer = Observer()
+
+observer.schedule(event_handler, from_dir, recursive=True)
+
+observer.start()
+
+try:
+    while True:
+        time.sleep(2)
+        print('executando...')
+except KeyboardInterrupt:
+    print('interrompido!')
+    observer.stop()
+
+
+
+
+
+
+
